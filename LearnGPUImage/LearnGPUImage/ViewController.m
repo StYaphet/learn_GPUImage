ed094ddac0233c6e72d1a347545c77e0fef2fbef
StYaphet <947670170@qq.com>
2018-08-09 08:50:50 +0800

    gpuimage输出图像

--- LearnGPUImage/LearnGPUImage/ViewController.m
+++ LearnGPUImage/LearnGPUImage/ViewController.m
@@ -7,8 +7,13 @@
 //
 
 #import "ViewController.h"
+#import "GPUImageVideoCamera.h"
+#import "GPUImageMotionBlurFilter.h"
+#import "GPUImageView.h"
 
 @interface ViewController ()
+
+@property (nonatomic, strong) GPUImageVideoCamera *camera;
 
 @end
 
@@ -16,8 +21,20 @@
 
 - (void)viewDidLoad {
 	[super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+	[self createCamera];
 }
 
+- (void)createCamera {
+	self.camera = [[GPUImageVideoCamera alloc] initWithSessionPreset:AVCaptureSessionPreset1280x720 cameraPosition:AVCaptureDevicePositionBack];
+	self.camera.outputImageOrientation = UIInterfaceOrientationPortrait;
+//	GPUImageFilter *filter = [[GPUImageMotionBlurFilter alloc] init];
+	GPUImageView *filteredVideoView = [[GPUImageView alloc] initWithFrame:self.view.bounds];
+	[self.view addSubview:filteredVideoView];
+	
+	[self.camera addTarget:filteredVideoView];
+//	[filter addTarget:filteredVideoView];
+	
+	[self.camera startCameraCapture];
+}
 
 @end
